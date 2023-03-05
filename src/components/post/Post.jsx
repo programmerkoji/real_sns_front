25b0d1c62920542fddc95d675ded2c75368371fc
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2023-03-05 15:02:59 +0900

    ダミーデータで投稿を作成

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,15 +1,18 @@
 import { MoreVert } from "@mui/icons-material";
 import React from "react";
 import "./Post.css";
+import { Users } from "../../dummyData";
 
 export default function Post({post}) {
+  const userInfo = Users.filter((user) => user.id === post.id);
+
 	return (
 		<div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
             <img src="./assets/person/1.jpeg" alt="" className="postProfileImg" />
-            <span className="postUsername">Shin Code</span>
+            <span className="postUsername">{ userInfo[0].username }</span>
             <span className="postDate">{ post.date }</span>
           </div>
           <div className="postTopRight">
@@ -22,7 +25,7 @@ export default function Post({post}) {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img src="./assets/heart.png" alt="" className="likeIcon" />
+            <img src={ userInfo[0].profilePicture } alt="" className="likeIcon" />
             <span className="postLikeCounter">{ post.like }がいいねを押しました</span>
           </div>
           <div className="postBottomRight">
